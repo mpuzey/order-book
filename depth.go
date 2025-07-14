61cdf1c998a0860bb3eac1ac09bd5f8b5d83731d
MattPuzey <mpuzey1@outlook.com>
2025-07-14 13:37:11 +0100

    move depth response to depth.go

--- depth.go
+++ depth.go
@@ -7,6 +7,12 @@ import (
 	"net/url"
 )
 
+type DepthResponse struct {
+	LastUpdateID int        `json:"lastUpdateId"`
+	Bids         [][]string `json:"bids"`
+	Asks         [][]string `json:"asks"`
+}
+
 func DepthRequestHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodGet {
--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 
 var orderBook = NewOrderBook()
 
-type DepthResponse struct {
-	LastUpdateID int        `json:"lastUpdateId"`
-	Bids         [][]string `json:"bids"`
-	Asks         [][]string `json:"asks"`
-}
-
 func main() {
 	http.HandleFunc("/api/v3/depth/", DepthRequestHandler)
 	http.ListenAndServe(":3001", nil)
