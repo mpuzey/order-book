10af6bb7d7a5558c8a99768dac53b55b7ebdf120
MattPuzey <mpuzey1@outlook.com>
2025-07-14 13:35:02 +0100

    working depth endpoint

--- depth.go
+++ depth.go
@@ -1,12 +1,32 @@
 package main
 
-type DepthRequest struct {
-	Symbol string `json:"symbol"`
-	Limit  *int   `json:"limit"`
-}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
-type DepthResponse struct {
-	LastUpdateID int        `json:"lastUpdateId"`
-	Bids         [][]string `json:"bids"`
-	Asks         [][]string `json:"asks"`
+func DepthRequestHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	q, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("symbol is:", q.Get("symbol"))
+
+	depthResponse := &DepthResponse{
+		LastUpdateID: orderBook.lastUpdateID,
+		Bids:         orderBook.GetBids(),
+		Asks:         orderBook.GetAsks(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(depthResponse)
+
 }
--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 var orderBook = NewOrderBook()
 
-func DepthRequestHandler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("testing!")
-	if r.Method == http.MethodGet {
-		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var req DepthRequest
-	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	depthResponse := &DepthResponse{
-		LastUpdateID: orderBook.lastUpdateID,
-		Bids:         orderBook.GetBids(),
-		Asks:         orderBook.GetAsks(),
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(depthResponse)
-
+type DepthResponse struct {
+	LastUpdateID int        `json:"lastUpdateId"`
+	Bids         [][]string `json:"bids"`
+	Asks         [][]string `json:"asks"`
 }
 
 func main() {
